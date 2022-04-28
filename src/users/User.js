ea92ee08f67f86448adc3d9731f11283314ca1aa
astolfovalentim <astolfom@gmail.com>
2022-04-28 16:05:08 -0300

    model User

---
+++ src/users/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+      },
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        /* select: false, */
+      },
+      photo: {
+        type: String,
+        required: true,
+
+      },
+    });
+
+    const User = mongoose.model("User, UserSchema")
+    module.exports = User
+
+
+
+
+
+
+
+
+
+})
