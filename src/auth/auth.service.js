55a90a2123bc65a0fdd351bdf80f8d6f0739fe55
astolfovalentim <astolfom@gmail.com>
2022-04-29 21:45:35 -0300

    alteracao sem bugs

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,8 +1,8 @@
 const User = require('../users/User');
 const jwt = require('jsonwebtoken');
 
-const loginService = async (email) =>
-  await User.findOne({ email: email }).select('+password');
+const loginService = (email) =>
+  User.findOne({ email: email }).select('+password');
 
 const generateToken = (userId) =>
   jwt.sign({ id: userId }, process.env.SECRET, { expiresIn: 86400 });
